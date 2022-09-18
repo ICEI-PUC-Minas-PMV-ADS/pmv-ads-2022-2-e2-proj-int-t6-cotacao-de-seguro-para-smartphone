7b46d23ce3c4f5cbb8f22a6ca590f64cae004a98
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-09-18 20:10:48 -0300

    criando footer

--- src/frontEnd/SeguroCelular.UI/src/app/app.module.ts
+++ src/frontEnd/SeguroCelular.UI/src/app/app.module.ts
@@ -5,19 +5,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
   ],
   exports: [
-    NavComponent
+    NavComponent,
+    FooterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
