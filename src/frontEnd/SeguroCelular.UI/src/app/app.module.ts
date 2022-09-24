a2fea8168d6f4eb5fff1d9df4e536c8e8a9ac4c9
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-09-24 14:11:39 -0300

    tela login funcionalidade basica

--- src/frontEnd/SeguroCelular.UI/src/app/app.module.ts
+++ src/frontEnd/SeguroCelular.UI/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,8 +22,9 @@ import { SobreComponent } from './components/sobre/sobre.component';
     SobreComponent
   ],
   imports: [
-    BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
+    BrowserModule
   ],
   exports: [
     NavComponent,
--- src/frontEnd/SeguroCelular.UI/src/app/components/login/login.component.ts
+++ src/frontEnd/SeguroCelular.UI/src/app/components/login/login.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: any;
+
+  emailDB: string = "email@teste.com";
+
+  senhaDB: string = "12345678";
+
+  mensagem: string = '';
+
+  btnSignIn: any;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.criarForm();
+  }
 
   ngOnInit(): void {
   }
 
+  criarForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]]
+    })
+  }
+
+  CadastrarUser() {
+
+  }
+
+  Login() {
+    if (this.loginForm.get('email').value == this.emailDB && this.loginForm.get('senha').value == this.senhaDB) {
+      this.router.navigate(["/home"]);;
+    } else {
+      this.mensagem = "E-mail ou a senha estar errado!";
+    }
+  }
 }
