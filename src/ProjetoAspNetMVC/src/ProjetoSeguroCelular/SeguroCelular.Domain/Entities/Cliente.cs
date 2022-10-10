2c834ad279a2ef70d64c9dc264a8f0a91d3e0db4
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-10-10 16:51:41 -0300

    criando entidade cliente

---
+++ src/ProjetoAspNetMVC/src/ProjetoSeguroCelular/SeguroCelular.Domain/Entities/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using SeguroCelular.Domain.ValueObjects;
+
+namespace SeguroCelular.Domain.Entities
+{
+    public class Cliente : Entity
+    {
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+        public string Cpf { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public string Email { get; private set; }
+        public Endereco Endereco { get; private set; }
+
+        public Cliente(string nome, string sobrenome, string cpf, DateTime dataNascimento, string email, Endereco endereco)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Cpf = cpf;
+            DataNascimento = dataNascimento;
+            Email = email;
+            Endereco = endereco;
+        }
+    }
+}
---
+++ src/ProjetoAspNetMVC/src/ProjetoSeguroCelular/SeguroCelular.Domain/Enums/Estados.cs
@@ -0,0 +1,32 @@
+﻿namespace SeguroCelular.Domain.Enums
+{
+    public enum Estados
+    {
+        AC,
+        AL,
+        AP,
+        AM,
+        BA,
+        CE,
+        DF,
+        ES,
+        GO,
+        MA,
+        MT,
+        MS,
+        MG,
+        PA,
+        PB,
+        PE,
+        PI,
+        RJ,
+        RN,
+        RS,
+        RO,
+        RR,
+        SC,
+        SP,
+        SE,
+        TO
+    }
+}
