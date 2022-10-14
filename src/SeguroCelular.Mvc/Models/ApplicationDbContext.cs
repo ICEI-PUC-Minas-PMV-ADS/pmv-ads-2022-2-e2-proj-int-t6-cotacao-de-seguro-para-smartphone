a5a20794857bd7220297b59b9353d69cb9d34c79
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-10-14 20:17:56 -0300

    configurando entity framork

---
+++ src/SeguroCelular.Mvc/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SeguroCelular.Mvc.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
--- src/SeguroCelular.Mvc/Startup.cs
+++ src/SeguroCelular.Mvc/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SeguroCelular.Mvc.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,13 +22,15 @@ namespace SeguroCelular.Mvc
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +40,6 @@ namespace SeguroCelular.Mvc
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
