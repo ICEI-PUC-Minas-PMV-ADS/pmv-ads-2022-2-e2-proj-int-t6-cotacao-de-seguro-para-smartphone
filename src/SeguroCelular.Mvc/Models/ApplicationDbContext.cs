3203a18ad53aaaac85902f7bade4eed81b07e04b
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-10-15 10:13:43 -0300

    feat:configuracao do entityframework

--- src/SeguroCelular.Mvc/Models/ApplicationDbContext.cs
+++ src/SeguroCelular.Mvc/Models/ApplicationDbContext.cs
@@ -4,8 +4,8 @@ namespace SeguroCelular.Mvc.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<Veiculo> Veiculos { get; set; }
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
     }
 }
